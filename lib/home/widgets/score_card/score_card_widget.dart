76208f913d5d8d264dd42a961183743de0278a77
erikbernard <erikbrsilva@gmail.com>
2021-04-19 16:41:17 -0300

    score card widget created

---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,58 @@
+import 'package:devquiz/core/core.dart';
+import 'package:devquiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  const ScoreCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Container(
+        height: 126,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            new BoxShadow(
+              color: AppColors.lightGrey,
+              blurRadius: 1.5,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 22, right: 20),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: ChartWidget(),
+              ),
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 24),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Vamos começar",
+                        style: AppTextStyles.heading,
+                      ),
+                      Text(
+                        "Complete os desafios e avance de conhecimento",
+                        style: AppTextStyles.body,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
