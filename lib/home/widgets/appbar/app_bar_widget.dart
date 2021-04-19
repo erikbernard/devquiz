2386af897f5fee152be55c98d9cf0b4888ddd941
erikbernard <erikbrsilva@gmail.com>
2021-04-19 16:40:44 -0300

    appbar widget created

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,56 @@
+import 'package:devquiz/core/core.dart';
+import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  height: 161,
+                  width: double.maxFinite,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                text: "Erik Bern",
+                                style: AppTextStyles.titleBold,
+                              ),
+                            ]),
+                      ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/76105702?s=400&u=c317b3261eee35596e2019b79a786172052f5a04&v=4"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0, 1),
+                  child: ScoreCardWidget(),
+                ),
+              ],
+            ),
+          ),
+        );
+}
