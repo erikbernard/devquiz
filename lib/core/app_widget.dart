1f78b1486edf5354d1962041d218c2f9d355b8ae
erikbernard <erikbrsilva@gmail.com>
2021-04-19 16:36:46 -0300

    project resource configuration

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,17 @@
+// import 'package:DevQuiz/home/home_page.dart';
+// import 'package:DevQuiz/splash/splash_page.dart';
+import 'package:devquiz/home/home_page.dart';
+import 'package:devquiz/main.dart';
+import 'package:devquiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
