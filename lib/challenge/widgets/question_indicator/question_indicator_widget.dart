54bac6799da4c8fc99d070ab107587d606332bee
erikbernard <erikbrsilva@gmail.com>
2021-04-25 13:40:57 -0300

    update de features challenge page

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -3,7 +3,13 @@ import 'package:devquiz/shared/widgets/progress_indicator/progress_indicator_wid
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
-  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+  final int currentPage;
+  final int length;
+  const QuestionIndicatorWidget({
+    Key? key,
+    required this.currentPage,
+    required this.length,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +23,17 @@ class QuestionIndicatorWidget extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "Questão 04",
+                  "Questão $currentPage",
                   style: AppTextStyles.body,
                 ),
                 Text(
-                  "de 10",
+                  "de $length",
                   style: AppTextStyles.body,
                 ),
               ],
             ),
             SizedBox(height: 16),
-            ProgressIndicatorWidget(value: 0.7)
+            ProgressIndicatorWidget(value: currentPage / length),
           ],
         ),
       ),
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challenge/challenge_page.dart';
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:devquiz/core/app_colors.dart';
 import 'package:devquiz/home/home_controller.dart';
@@ -66,12 +67,24 @@ class _HomePageState extends State<HomePage> {
                   crossAxisSpacing: 16,
                   mainAxisSpacing: 16,
                   children: controller.quizzes!
-                      .map((e) => QuizCardWidget(
-                            title: e.title,
-                            percent: e.questionAnswered / e.questions.length,
-                            completed:
-                                "${e.questionAnswered}/${e.questions.length}",
-                          ))
+                      .map(
+                        (e) => QuizCardWidget(
+                          title: e.title,
+                          percent: e.questionAnswered / e.questions.length,
+                          completed:
+                              "${e.questionAnswered}/${e.questions.length}",
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChallengePage(
+                                  questions: e.questions,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
                       .toList(),
                 ),
               ),
